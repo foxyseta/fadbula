b6cf6928aa5762be5ae91598de4441519b506913
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2023-01-01 19:09:44 +0100

    global CSV export

---
+++ src/operation-05.sql
@@ -0,0 +1,7 @@
+-- Operazione 5: inserire nuovi luoghi
+
+INSERT INTO Luogo(Nome, X, Y, Mappa)
+VALUES
+		("Casa di Cappuccetto", 0.1, 0.1, 1),
+		("Bosco", 0.5, 0.5, 1),
+		("Casa della nonna", 0.9, 0.9, 1)
