d88a7937a37fe4a327097814b3ac8e0e3ec11783
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2023-01-01 18:56:53 +0100

    up to operation 3

---
+++ src/operation-02.sql
@@ -0,0 +1,9 @@
+-- Operazione 2: inserire nuove fasi
+
+INSERT INTO Fase(Indice, Nome)
+VALUES
+		(1, 'Aurora'),
+		(2, 'Alba'),
+		(3, 'Prima mattina'),
+		(4, 'Metà mattina'),
+		(5, 'Mattina inoltrata')
