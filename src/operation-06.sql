0ddf96e8dcf374c0c805cf885e83fe037a393182
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2023-01-02 11:05:58 +0100

    Flashback e analessi + eventi)

--- src/operation-06.sql
+++ src/operation-06.sql
@@ -1,3 +1,13 @@
 -- Operazione 6: inserire nuovi eventi
 INSERT INTO Evento(Codice, Nome, Veridicita)
-VALUES (0, "Mamma chiede un favore", TRUE)
+VALUES
+	(0, "Mamma chiede un favore", TRUE),
+	(1, "Mamma mette in guardia", TRUE),
+	(2, "Cappuccetto parte", TRUE),
+	(3, "Cappuccetto incontra Lupo", TRUE),
+	(4, "Lupo raggiunge nonna", TRUE),
+	(5, "Lupo mangia nonna", TRUE),
+	(6, "Cappuccetto raggiunge nonna", TRUE),
+	(7, "Nonna è a letto", FALSE),
+	(8, "Lupo mangia cappuccetto", TRUE),
+	(9, "Arriva il cacciatore", TRUE)
